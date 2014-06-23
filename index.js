1118968e4edad7efef64daad9c92093c01b939ab
mmbash <mike.michel811@gmail.com>
2014-06-23 15:10:18 +0200

    all new

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello from inside a container!');
+});
+
+app.listen(8080);
